c0ba76d7ca08b561049f5200deec61c6dbd20714
Tao Lin <tao.lin@accordo.com>
2018-03-25 20:43:46 +1300

    Make https://github.com/sbalay/react-native-redux-form-example working with latest react-native

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import main from './src/main';
 
-AppRegistry.registerComponent('MyApp', () => App);
+AppRegistry.registerComponent('MyApp', () => main);
